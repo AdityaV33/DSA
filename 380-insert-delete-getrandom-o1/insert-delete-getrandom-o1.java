8d1f3c4e0860354413862fef847ec9f28d3c4ba9
Aditya Verma <vermaaditya457@gmail.com>
2026-06-06 23:53:47 +0530

    Time: 24 ms (99.98%) | Memory: 100.6 MB (37.36%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,53 +1,42 @@
 class RandomizedSet {
+    private ArrayList<Integer> list;
+    private HashMap<Integer, Integer> map;
+    private Random random;
 
-List<Integer> list;
-HashMap<Integer,Integer> map;
-Random random = new Random();
+    
     public RandomizedSet() {
-        list = new ArrayList<Integer>();
-        map = new HashMap<Integer,Integer>();
-        
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        random = new Random();
     }
     
+   
     public boolean insert(int val) {
-        if(map.containsKey(val)){
+        if (map.containsKey(val)) {
             return false;
         }
-        map.put(val,list.size());
         list.add(val);
+        map.put(val, list.size() - 1);
         return true;
-
     }
     
-    public boolean remove(int val) { 
-        if(!map.containsKey(val)){
+   
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
             return false;
         }
-        int pos = map.get(val);
-        if(pos!=(list.size())-1){
-             int lastelem=list.get(list.size()-1);
-             list.set(pos,lastelem);
-             map.put(lastelem,pos);
-
-        }
+        int idx = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+        list.set(idx, lastElement);
+        map.put(lastElement, idx);
+        list.remove(list.size() - 1);
         map.remove(val);
-        list.remove(list.size()-1);
         return true;
-        
     }
+    
     
     public int getRandom() {
-        int randomInt =random.nextInt(list.size());
-        return list.get(randomInt);
-
-        
+        int idx = random.nextInt(list.size());
+        return list.get(idx);
     }
 }
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
