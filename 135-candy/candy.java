8df98d5a0356441da10fedc9537da0b522b95bad
Aditya Verma <vermaaditya457@gmail.com>
2026-06-18 18:36:54 +0530

    Time: 3 ms (89.01%) | Memory: 47.7 MB (50.14%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int candy = n;
+
+        int i = 1 ; 
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
+
+            }
+
+        
+        int peak = 0;
+        while(i<n&&ratings[i]>ratings[i-1]){
+            peak++;
+            candy +=peak;
+            i++;
+        }
+        int dip = 0;
+        while(i<n&& ratings [i]<ratings[i-1]){
+            dip++;
+            candy+=dip;
+            i++;
+
+        }
+        candy -= Math.min(dip,peak);
+    }
+    return candy;
+    }
+}
