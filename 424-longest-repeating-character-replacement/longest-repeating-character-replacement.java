113f43e7bb624baf1345e72ca57271e120aa08da
Aditya Verma <vermaaditya457@gmail.com>
2026-08-03 00:55:46 +0530

    Time: 7 ms (89.13%) | Memory: 46.2 MB (65.44%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,30 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int freq[] = new int[26];
-        int left = 0;
-        int maxfreq =0 ; 
-        int maxWindow=0;
+        int [] freq = new int [26];
 
-        for(int right =0  ; right<s.length();right++){
-            //Update the frequency of current character
-            freq[s.charAt(right)-'A']++;
-            //update the max frequency
-            maxfreq=Math.max(maxfreq,freq[s.charAt(right)-'A']);
+        int left = 0 ;
+        int maxFreq = 0;
+        int maxlen = 0;
+
+        for(int right = 0 ; right<s.length(); right++ ){
             
-            int windowslength = right -left +1;
+            freq[s.charAt(right)-'A']++;
 
-            //if windowslength - maxfreq > k 
-            //we need to shrink
-            if(windowslength - maxfreq>k) {
+            maxFreq = Math.max(maxFreq, freq[s.charAt(right)-'A']);
+                // size of subarray  -max freq > k ( meaans youve got more than k to change hence shrink )
+            while((right - left+1)-maxFreq>k){
                 freq[s.charAt(left)-'A']--;
                 left++;
-
             }
+            maxlen = Math.max(maxlen, right - left + 1);
 
-            windowslength = right -left +1;
-            maxWindow = Math.max(maxWindow,windowslength);
         }
-        return maxWindow;
-        
+        return maxlen;
     }
 }
