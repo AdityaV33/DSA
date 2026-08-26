debb6489ac3cf1d3015c3d5ff6baf21ebe708ce0
Aditya Verma <vermaaditya457@gmail.com>
2026-08-27 00:18:37 +0530

    Time: 4 ms (36.57%) | Memory: 43.7 MB (7.22%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,31 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()%2!=0) return false;
-        Stack<Character> stack  = new Stack<>();
-
-        for(int i = 0 ; i<s.length();i++){
-            char c = s.charAt(i);
-            if(c=='('||c=='{'||c=='['){
-                stack.push(c);
-
-
-            }
-            else if(c==')'&& !stack.isEmpty()&& stack.peek()=='('){
-                stack.pop();
-            }
-            else if(c=='}'&& !stack.isEmpty()&& stack.peek()=='{'){
-                stack.pop();
-            }
-            else if(c==']'&& !stack.isEmpty()&& stack.peek()=='['){
-                stack.pop();
-            }
-            else { 
-                return false ;
-            }
+        Stack <Character> st = new Stack <>();
+        for(int i =0 ;i<s.length();i++){
+            if(s.charAt(i)=='('||
+               s.charAt(i)=='{'||
+               s.charAt(i)=='[' ){
+                st.push(s.charAt(i));
+               }
+               else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                if(s.charAt(i) == ')' && st.peek()!='('){
+                    return false;
+                }
+                if(s.charAt(i) == '}' && st.peek()!='{'){
+                    return false;
+                }
+                if(s.charAt(i)==']'&& st.peek()!='['){
+                    return false;
+                }
+                st.pop();
+               }
         }
+        return st.isEmpty();
 
-        return stack.isEmpty();
 
         
+        }
+        
     }
-}
