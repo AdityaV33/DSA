cd80a5a6f471a9a486436c01262f8e2225278183
Aditya Verma <vermaaditya457@gmail.com>
2026-08-29 23:16:24 +0530

    Time: 33 ms (72.05%) | Memory: 59.1 MB (74.43%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,32 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
+        int i = 0 ;
+    
+        List<List<Integer>> l = new ArrayList<>();
         Arrays.sort(nums);
+        for(i = 0 ;  i<nums.length -2 ; i++){
+            int j = i+1;
+            int k  = nums.length-1;
+            if(i>0 && nums[i-1]==nums[i] ) continue;
 
-        for(int i =0 ; i<nums.length -2 ;i++){
-            if(i>0 && nums[i-1]==nums[i]) continue;
-        
 
-        int j = i+1;
-        int k = nums.length -1 ;
+            while(j<k){
+                int sum  =  nums[i]+nums[j]+nums[k];
+                if(sum >0  ) k--;
+                else if (sum<0)j++;
+                else{ 
+                    l.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;
+                    k--;
 
-        while(j<k){
-            int sum = nums[i]+nums[j]+nums[k];
-            if(sum<0) j++;
-            else if(sum>0)k--;
-            else{
-
-                ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                j++;
-                k--;
-                //skip duplicates
-                while(j<k && nums[j]==nums[j-1]) j++;
-                while(j<k && nums[k]==nums[k+1]) k--;
+                    while(j<k && nums[j-1]==nums[j]) j++;
+                    while(j<k && nums[k+1]==nums[k]) k--;
+                }
 
             }
         }
+        return l;
+            
         }
-        return ans;
+        
     }
-}
